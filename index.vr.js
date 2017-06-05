af1efc6756e03f0daf2fe4eded75334632ddb66d
github username <github email address>
2017-06-04 21:15:01 -0400

    first commit

---
+++ index.vr.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, Text, View, Model, AmbientLight} from 'react-vr';
+
+
+class Planet extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      translate: [0,0,0],
+      scale: 0.05,
+      rotateX: -10,
+      rotateY: 0,
+      rotateZ: -10,
+      obj: 'earth.obj',
+      mtl: 'earth.mtl'
+    }
+  }
+
+  componentWillMount(){
+    this.setState({
+      translate: this.props.translate,
+      obj: this.props.obj,
+      mtl: this.props.mtl
+    })
+  }
+  render(){
+    // positioning
+    let translate = this.state.translate
+    let scale = this.state.scale
+    let x = this.state.rotateX
+    let y = this.state.rotateY
+    let z = this.state.rotateZ
+    // object and texture
+    let obj = this.state.obj
+    let mtl = this.state.mtl
+
+    return(
+      <View>
+        <AmbientLight intensity={ 2.6 } />
+        <Model
+          style={{
+            transform: [ {translate: translate},{scale: scale },{rotateY: y},{rotateX: x},{rotateZ: z} ],
+          }}
+          source={{obj:asset(obj), mtl:asset(mtl)}}
+          lit={true} />
+      </View>
+    )
+  }
+}
+
+class PlanetContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      planets: {
+        'earth': <Planet name="earth" translate={[-50,0,0]} obj='earth.obj' mtl='earth.mtl'/>
+      }
+    }
+  }
+
+  render(){
+    let planets = this.state.planets
+    let planet = this.props.planet
+    let description = this.props.description
+    return (
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        width: 50,
+        alignItems: 'stretch',
+        transform: [{translate:[0,0,-70]}]
+      }}>
+
+        <View style={{ margin: 0.1, height: 0.3}}>
+           {planets[planet]}
+        </View>
+
+        <View style={{ margin: 0.1, height: 3, transform: [{translate:[0,10,0]}]}}>
+          <Text style={{fontSize: 4, textAlign: 'center'}}>{ planet }</Text>
+        </View>
+
+        <View style={{ margin: 0.1, height: 10, transform: [{translate:[0,8,0]}]}}>
+          <Text style={{fontSize: 2, textAlign: 'center'}}>{ description }</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+class PlanetsVR extends Component {
+  render() {
+    return (
+      <View>
+        <View style={{
+          transform: [{translate:[10,0,10]},{rotateY:0}]
+        }}>
+          {/* <Pano source={asset(this.state.background)}/> */}
+          <PlanetContainer planet="earth" description="this is a description of earth"/>
+        </View>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('PlanetsVR', () => PlanetsVR);
